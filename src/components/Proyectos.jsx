996d6966fb7d50b5103805c71c6a4ac92ea7c035
hectordani03 <hectordaniel010203@gmail.com>
2024-04-06 23:51:32 -0600

    routing img version 2

--- src/components/Proyectos.jsx
+++ src/components/Proyectos.jsx
@@ -1,37 +1,40 @@
 import { CardProyects } from './CardProyect';
+import ForUs from '../assets/For-Us.png'
+import Cinehub from '../assets/Sportx.png'
+import SportX from '../assets/cinehub-2.jpeg'
 
 export const Proyectos = () => {
     const modalImagesForUs = [
         {
             label: 'HomePage',
-            imgPath: '/src/assets/For-Us.png',
+            imgPath: '../assets/For-Us.png',
         },
         {
             label: 'Posts',
-            imgPath: '/src/assets/For-Us-2.png',
+            imgPath: '../assets/For-Us-2.png',
             
         },
         {
             label: 'Profile',
-            imgPath: '/src/assets/For-Us-4.png',
+            imgPath: '../assets/For-Us-4.png',
         },
     ];
 
     const modalImagesSportX = [
         {
             label: 'DashBoard',
-            imgPath: '/src/assets/SportX.png',
+            imgPath: '../assets/SportX.png',
         },
     ];
 
     const modalImagesCineHub = [
         {
             label: 'HomePage',
-            imgPath: '/src/assets/Cinehub.jpeg',
+            imgPath: '../assets/Cinehub.jpeg',
         },
         {
             label: 'Posts',
-            imgPath: '/src/assets/Cinehub-2.jpeg',
+            imgPath: '../assets/Cinehub-2.jpeg',
         },
     ];
 
@@ -40,19 +43,19 @@ export const Proyectos = () => {
             <h1 className=' mt-20 text-center mb-10 text-3xl sm:text-4xl md:text-5xl  font-bold w-12/12 dark:text-slate-300'>Nuestros<span className='text-emerald-400 ml-2 lg:ml-4'>Proyectos</span></h1>
             <div className="grid gap-8 justify-items-center grid-cols-1 xl:grid-cols-2">
                 <CardProyects
-                    src={"./assets/For-Us.png"}
+                    src={ForUs}
                     title={"For Us"}
                     info={"Foro Web de discusiones sobre objetivos de la Onu"}
                     modalImages={modalImagesForUs}
                 />
                 <CardProyects
-                    src={"assets/Sportx.png"}
+                    src={SportX}
                     title={"For Us"}
                     info={"Aplicacion Web sobre un sistema de gestion de almacen"}
                     modalImages={modalImagesSportX}
                 />
                 <CardProyects
-                    src={"../assets/cinehub-2.jpeg"}
+                    src={Cinehub}
                     title={"For Us"}
                     info={"Blog Web sobre Cine"}
                     modalImages={modalImagesCineHub}
